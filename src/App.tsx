8baf1a1594cf473992368fb8e60203a611f012f5
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2024-02-23 19:43:42 -0500

    New Version of Social Media App

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+import AuthLayout from './_auth/AuthLayout';
+import SignupForm from "./_auth/forms/SignupForm";
+import SigninForm from "./_auth/forms/SigninForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+    <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignupForm />} />
+          <Route path="/sign-up" element={<SigninForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout  />}>
+           <Route index element={<Home />} />
+        </Route>
+       </Routes>
+    </main>
+  )
+}
+
+export default App
