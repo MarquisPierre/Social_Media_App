35171536683606e362324c1c28fed69b2a1b2818
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2024-06-10 21:44:02 -0400

    dont know what i did but im just testing

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
-import {Input}  from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
 import useDebounce from "@/hooks/useDebounce";
 import GridPostList from "@/components/ui/shared/GridPostLists";
-import Loader from "@/components/ui/shared/Loader"
+import Loader from "@/components/ui/shared/Loader";
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations";
 
 export type SearchResultProps = {
@@ -36,7 +36,7 @@ const Explore = () => {
     if (inView && !searchValue) {
       fetchNextPage();
     }
-  }, [inView, searchValue]);
+  }, [inView, searchValue, fetchNextPage]);
 
   if (!posts)
     return (
@@ -46,8 +46,7 @@ const Explore = () => {
     );
 
   const shouldShowSearchResults = searchValue !== "";
-  const shouldShowPosts = !shouldShowSearchResults && 
-    posts.pages.every((item) => item.documents.length === 0);
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item && item.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -96,9 +95,11 @@ const Explore = () => {
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
-          posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
-          ))
+          posts.pages.map((item, index) =>
+            item ? (
+              <GridPostList key={`page-${index}`} posts={item.documents} />
+            ) : null
+          )
         )}
       </div>
 
